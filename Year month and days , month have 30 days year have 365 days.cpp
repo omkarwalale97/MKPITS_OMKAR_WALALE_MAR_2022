ec79d8dc10c28403a9e2457cfe62a12c426c3985
omkarwalale97 <101812309+omkarwalale97@users.noreply.github.com>
2022-04-13 13:16:29 +0530

    Add files via upload

---
+++ Year month and days , month have 30 days year have 365 days.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+{
+	int ndays, y,m,d;
+	printf("input no of days : ");
+	scanf("%d", &ndays);
+	y= (int)ndays/365;
+	ndays= ndays-(365*y);
+	m=(int)ndays/30;
+	d=(int)ndays-(m*30);
+	printf("\n %d year (s) \n %d month(s) \n %d days (s)",y,m,d);
+	return 0 ;
+}
