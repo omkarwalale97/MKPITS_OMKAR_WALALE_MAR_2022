035907860a17543038064e4ad35962359d22b145
omkarwalale97 <101812309+omkarwalale97@users.noreply.github.com>
2022-04-13 13:13:33 +0530

    Add files via upload

---
+++ Hello wrold omkar .c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : omkar parashram walale\n"); 
+     printf("DOB    : may 23 1997\n"); 
+     printf("Mobile : 8605962039\n"); 
+     return(0); 
+  }
+
