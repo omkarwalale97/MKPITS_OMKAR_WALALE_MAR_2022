b54010f406b24fef7d408e00e00cdad968e1ae32
omkarwalale97 <101812309+omkarwalale97@users.noreply.github.com>
2022-05-02 10:50:13 +0530

    Add files via upload

---
+++ pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int r,sp,str;
+	for(r=1;r<=4;r++)
+	{
+		for(sp=1;sp<=41-r;sp++)
+		{
+		printf(" ");
+	    }
+		for(str=1;str<=r;str++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
+
